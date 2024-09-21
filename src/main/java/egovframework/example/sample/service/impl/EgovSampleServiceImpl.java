ffa91c51b298d82b074a58c1df550943bf86d720
kang <kfor11@naver.com>
2024-09-21 10:40:02 +0900

    Source > Format

--- src/main/java/egovframework/example/sample/service/impl/EgovSampleServiceImpl.java
+++ src/main/java/egovframework/example/sample/service/impl/EgovSampleServiceImpl.java
@@ -17,34 +17,35 @@ package egovframework.example.sample.service.impl;
 
 import java.util.List;
 
-import egovframework.example.sample.service.EgovSampleService;
-import egovframework.example.sample.service.SampleDefaultVO;
-import egovframework.example.sample.service.SampleVO;
+import javax.annotation.Resource;
 
 import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
 import org.egovframe.rte.fdl.idgnr.EgovIdGnrService;
-
-import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import egovframework.example.sample.service.EgovSampleService;
+import egovframework.example.sample.service.SampleDefaultVO;
+import egovframework.example.sample.service.SampleVO;
 
 /**
  * @Class Name : EgovSampleServiceImpl.java
  * @Description : Sample Business Implement Class
  * @Modification Information
- * @
- * @  수정일      수정자              수정내용
- * @ ---------   ---------   -------------------------------
- * @ 2009.03.16           최초생성
+ * 
+ *               <pre>
+ *   수정일      수정자              수정내용
+ *  ---------   ---------   -------------------------------
+ *  2009.03.16           최초생성
+ *               </pre>
  *
  * @author 개발프레임웍크 실행환경 개발팀
  * @since 2009. 03.16
  * @version 1.0
  * @see
  *
- *  Copyright (C) by MOPAS All right reserved.
+ *      Copyright (C) by MOPAS All right reserved.
  */
 
 @Service("sampleService")
@@ -53,7 +54,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 	private static final Logger LOGGER = LoggerFactory.getLogger(EgovSampleServiceImpl.class);
 
 	/** SampleDAO */
-	@Resource(name="sampleMapper")
+	@Resource(name = "sampleMapper")
 	private SampleMapper sampleDAO;
 
 	/** ID Generation */
@@ -62,6 +63,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글을 등록한다.
+	 * 
 	 * @param vo - 등록할 정보가 담긴 SampleVO
 	 * @return 등록 결과
 	 * @exception Exception
@@ -81,6 +83,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글을 수정한다.
+	 * 
 	 * @param vo - 수정할 정보가 담긴 SampleVO
 	 * @return void형
 	 * @exception Exception
@@ -92,6 +95,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글을 삭제한다.
+	 * 
 	 * @param vo - 삭제할 정보가 담긴 SampleVO
 	 * @return void형
 	 * @exception Exception
@@ -103,6 +107,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글을 조회한다.
+	 * 
 	 * @param vo - 조회할 정보가 담긴 SampleVO
 	 * @return 조회한 글
 	 * @exception Exception
@@ -117,6 +122,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글 목록을 조회한다.
+	 * 
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return 글 목록
 	 * @exception Exception
@@ -128,6 +134,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글 총 갯수를 조회한다.
+	 * 
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return 글 총 갯수
 	 * @exception
